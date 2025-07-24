4c71c686e12d6ba6bfe6ccdfd4c8e1f2c7622438
jejusamds <160725458+jejusamds@users.noreply.github.com>
2025-07-24 10:20:43 +0900

    Update member_signup.php

--- controller/member_signup.php
+++ controller/member_signup.php
@@ -33,6 +33,14 @@ foreach($required as $field){
 $row = $db->row("SELECT COUNT(*) AS cnt FROM df_site_member WHERE f_user_id = :id", ['id'=>$_POST['f_user_id']]);
 if($row['cnt']) json_exit(['result'=>'error','msg'=>'이미 사용중인 아이디입니다.']);
 
+// 휴대폰 번호 중복확인
+$row = $db->row("SELECT COUNT(*) AS cnt FROM df_site_member WHERE f_mobile = :mobile", ['mobile'=>$_POST['f_mobile']]);
+if($row['cnt']) json_exit(['result'=>'error','msg'=>'이미 사용중인 휴대폰 번호입니다.']);
+
+// 이메일 중복확인
+$row = $db->row("SELECT COUNT(*) AS cnt FROM df_site_member WHERE f_email = :email", ['email'=>$_POST['f_email']]);
+if($row['cnt']) json_exit(['result'=>'error','msg'=>'이미 사용중인 이메일입니다.']);
+
 $tel    = $_POST['f_tel']    ?? '';
 $data = [
     'f_user_id'     => $_POST['f_user_id'],
@@ -55,4 +63,4 @@ if(!$db->query($sql,$data)){
     json_exit(['result'=>'error','msg'=>'저장 중 오류가 발생했습니다.']);
 }
 
-json_exit(['result'=>'ok','redirect'=>'/member/join_step03.html']);
+json_exit(['result'=>'ok','redirect'=>'/member/join_step03.html', 'msg'=>'회원가입이 완료되었습니다.']);
